94525a5ced04eef3cff6d0b3497ad52681f5cb23
fariqmanaf <fariqmanaf24@gmail.com>
2024-09-03 18:39:55 +0700

    feat: task validator

--- src/app/task/task.controller.js
+++ src/app/task/task.controller.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const { getListTasksService, findTaskService, createTaskService, updateTaskService, deleteTaskService } = require("./task.service");
 const authMiddleware = require('../../middleware/auth');
 const verifyProject = require("../../utils/handler/projectHandler");
+const taskValidator = require("./task.validator");
+const { task } = require('../../db');
 
 router.get("/:projectId/tasks", authMiddleware, verifyProject, getListTasksService);
 router.get("/:projectId/tasks/:id", authMiddleware, verifyProject, findTaskService);
-router.post("/:projectId/tasks", authMiddleware, verifyProject, createTaskService);
-router.put("/:projectId/tasks/:id", authMiddleware, verifyProject, updateTaskService);
+router.post("/:projectId/tasks", authMiddleware, taskValidator, verifyProject, createTaskService);
+router.put("/:projectId/tasks/:id", authMiddleware, taskValidator, verifyProject, updateTaskService);
 router.delete("/:projectId/tasks/:id", authMiddleware, verifyProject, deleteTaskService);
 
 module.exports = router;
--- src/app/task/task.validator.js
+++ src/app/task/task.validator.js
@@ -1,1 +1,13 @@
 const asyncHandler = require("../../utils/handler/asyncHandler");
+
+const taskValidator = asyncHandler(async (req, res, next) => {
+  const { name, description } = req.body;
+
+  if(!name || !description) {
+    return res.status(400).json({ message: "Name and description are required" });
+  }
+
+  next();
+});
+
+module.exports = taskValidator;
